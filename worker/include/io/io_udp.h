e7f00e314d18818970d562a882d94ab088016382
qw225967 <396139919@qq.com>
2022-10-19 15:54:26 +0800

    [feature] add io_udp

---
+++ worker/include/io/io_udp.h
@@ -0,0 +1,22 @@
+/*******************************************************
+ * @author      : dog head
+ * @date        : Created in 2022/10/19 2:42 下午
+ * @mail        : qw225967@github.com
+ * @project     : Bifrost
+ * @file        : io_udp.h
+ * @description : TODO
+ *******************************************************/
+
+#ifndef BIFROST_IO_UDP_H
+#define BIFROST_IO_UDP_H
+
+#include <uv.h>
+
+namespace bifrost {
+class IoUDP {
+ public:
+  class IoUDPListener {};
+};
+}  // namespace bifrost
+
+#endif  // BIFROST_IO_UDP_H
