c480319ed41ccf3917112fd8c616b90cb7875fa9
qw225967 <396139919@qq.com>
2023-11-08 21:12:40 +0800

    [chored] fix sh run

--- worker/src/bifrost/experiment_manager/ns3-interface/endpoint/endpoint-run.sh
+++ worker/src/bifrost/experiment_manager/ns3-interface/endpoint/endpoint-run.sh
@@ -6,28 +6,54 @@ ethtool -K eth0 tx off
 route del default
 route add default gw $GATEWAY
 
+udp_in_server=""
+udp_in_listen=""
+udp_out_server=""
+udp_out_listen=""
+
+if [ $1 = "client" ];then
+  # 配置输入的流向
+  udp_in_server="10.100.0.100:8889"
+  udp_in_listen="10.0.0.100:8889"
+  # 配置输出的流向
+  udp_out_server="10.0.0.2:9999"
+  udp_out_listen="10.0.0.100:8887"
+elif [ $1 = "server" ]; then
+  # 配置输入的流向
+  udp_in_server="10.0.0.100:8887"
+  udp_in_listen="10.100.0.100:8887"
+  # 配置输出的流向
+  udp_out_server="10.100.0.2:7777"
+  udp_out_listen="10.100.0.100:8889"
+else
+  echo "endpoint name err!"
+fi
+
+
 # 修改 nginx 配置，设置 in 代理到对端，out 代理到物理机中的IP(在bifrost的ns3代码中已写有)
 echo "stream {
           upstream udp_in {
-              server 10.100.0.100:8889;
+              server $udp_in_server;
           }
           server {
-              listen 10.0.0.100:8889 udp;
+              listen $udp_in_listen udp;
               proxy_responses 1;
               proxy_timeout 20s;
               proxy_pass udp_in;
               error_log /var/log/nginx/dns.log;
           }
           upstream udp_out {
-              server 10.0.0.2:9999;
+              server $udp_out_server;
           }
           server {
-              listen 10.100.0.100:8887 udp;
+              listen $udp_out_listen udp;
               proxy_responses 1;
               proxy_timeout 20s;
               proxy_pass udp_out;
               error_log /var/log/nginx/dns.log;
           }
       }" >> /etc/nginx/nginx.conf
+
+nginx
 
 sleep 3600
--- worker/src/bifrost/experiment_manager/ns3-interface/setup.sh
+++ worker/src/bifrost/experiment_manager/ns3-interface/setup.sh
@@ -20,9 +20,9 @@ echo "Building endpoint container..."
 docker build . -t endpoint
 echo "Running server and client container..."
 
-# 配置两个端口容器的网络，并且暴露udp端口对外
-docker run -d --cap-add=NET_ADMIN --network leftnet --ip 10.0.0.100 -e GATEWAY="10.0.0.2" -p 8889:8889/udp --name client endpoint
-docker run -d --cap-add=NET_ADMIN --network rightnet --ip 10.100.0.100 -e GATEWAY="10.100.0.2"  -p 8887:8887/udp --name server endpoint
+# 配置两个端口容器的网络，并且暴露udp端口对外，传入两个端口的名字判断 nginx 参数输入
+docker run -d --cap-add=NET_ADMIN --network leftnet --ip 10.0.0.100 -e GATEWAY="10.0.0.2" -p 8889:8889/udp -e PARAMS="client" --name client endpoint
+docker run -d --cap-add=NET_ADMIN --network rightnet --ip 10.100.0.100 -e GATEWAY="10.100.0.2"  -p 8887:8887/udp -e PARAMS="server" --name server endpoint
 cd ..
 
 echo "Starting NS3 container"
